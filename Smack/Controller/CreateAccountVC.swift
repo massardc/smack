c8a5488aa327a231146e37020eeb5e2cdceaece3
Clement M <massardc@users.noreply.github.com>
2017-09-10 23:19:17 +0200

    Set up register user function

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -9,9 +9,34 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    // Outlets
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userAvatar: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // Actions
+    @IBAction func createAccountBtnPressed(_ sender: Any) {
+        guard let email = emailTxt.text, emailTxt.text != "" else { return }
+        guard let password = passwordTxt.text, passwordTxt.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("Registered user!")
+            }
+        }
+    }
+    
+    @IBAction func chooseAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func generateBackgrdBtnPressed(_ sender: Any) {
+    }
+    
     
     @IBAction func closeBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND_TO_CHANNEL, sender: nil)
--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -7,7 +7,18 @@
 
 import Foundation
 
+typealias CompletionHandle = (_ Success: Bool) -> ()
+
+// URLs
+let BASE_URL = "http://localhost:3005/v1/"
+let REGISTER_URL = BASE_URL + "account/register"
+
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND_TO_CHANNEL = "unwindToChannel"
+
+// User defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
