13e6245f254a3c4608340225f22dde84bafd7abe
Clement M <massardc@users.noreply.github.com>
2017-11-04 18:27:25 +0100

    Implemented channel struct and call to retrieve data

---
+++ Smack/Model/Channel.swift
@@ -0,0 +1,16 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by ClementM on 04/11/2017.
+//  Copyright © 2017 ClementM. All rights reserved.
+//
+
+import Foundation
+
+struct Channel: Decodable {
+    public private(set) var _id: String!
+    public private(set) var name: String!
+    public private(set) var escription: String!
+    public private(set) var __v: Int?
+}
